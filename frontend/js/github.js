1ee828f1c0b6f815d5b8f62bdbe1e667d04e64b6
Jezebel1990 <jezebelguedes@gmail.com>
2025-11-20 19:03:41 -0300

    feat: add dynamic backend URL for local and production environments

--- frontend/js/github.js
+++ frontend/js/github.js
@@ -1,9 +1,14 @@
 const container = document.getElementById("projects");
 const searchInput = document.getElementById("searchInput");
 
+// Define a URL do backend dependendo do ambiente
+const BACKEND_BASE = window.location.hostname === "localhost"
+  ? "http://localhost:4000"
+  : "https://devops-portfolio-2t1i.onrender.com";
+
 async function loadRepos() {
   try {
-    const res = await fetch("http://localhost:4000/api/github");
+    const res = await fetch(`${BACKEND_BASE}/api/github`);
     if (!res.ok) throw new Error("Erro ao buscar dados");
     const data = await res.json();
     return data.elements;
@@ -26,14 +31,14 @@ function renderRepos(list) {
 
     container.innerHTML += `
     <div class="card">
-    <h3>${repo.name}</h3>
-    <p>${repo.description || "Sem descrição"}</p>
-    <p class="topics"><strong>Tecnologias:</strong> ${topics}</p>
-    <a href="${repo.html_url}" target="_blank" class="github-link">
-    <span>Abrir no</span>
-  <i class="fab fa-github fa-2x"></i>
-   </a>
-  </div>
+      <h3>${repo.name}</h3>
+      <p>${repo.description || "Sem descrição"}</p>
+      <p class="topics"><strong>Tecnologias:</strong> ${topics}</p>
+      <a href="${repo.html_url}" target="_blank" class="github-link">
+        <span>Abrir no</span>
+        <i class="fab fa-github fa-2x"></i>
+      </a>
+    </div>
     `;
   });
 }
@@ -48,6 +53,7 @@ function setupSearch(repos) {
   });
 }
 
+// Inicializa carregamento
 loadRepos().then(repos => {
   renderRepos(repos);
   setupSearch(repos);
